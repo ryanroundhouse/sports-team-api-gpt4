61f56474510bc1134e52eef2cb9b76c765bb71b8
Ryan Graham <rg@ryangraham.ca>
2023-04-23 20:51:18 -0400

    added missing teamMembershipRoutes tests

--- src/routes/teamMembershipRoutes.test.ts
+++ src/routes/teamMembershipRoutes.test.ts
@@ -1,27 +1,27 @@
-import supertest from 'supertest';
-import express from 'express';
-import initDatabase from '../db';
-import setupTeamMembershipRoutes from './teamMembershipRoutes';
-import { Database } from 'sqlite';
-import { generateToken, hashPassword } from '../auth';
+import supertest from 'supertest'
+import express from 'express'
+import initDatabase from '../db'
+import setupTeamMembershipRoutes from './teamMembershipRoutes'
+import { Database } from 'sqlite'
+import { generateToken, hashPassword } from '../auth'
 import {
   createTeamMembership,
   getTeamMembershipByTeamAndPlayer,
-} from '../dataAccess/teamMembershipData';
+} from '../dataAccess/teamMembershipData'
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-let db: Database;
+let db: Database
 
 beforeAll(async () => {
-  db = await initDatabase();
-  app.use(setupTeamMembershipRoutes(db));
-});
+  db = await initDatabase()
+  app.use(setupTeamMembershipRoutes(db))
+})
 
 afterAll(async () => {
-  await db.close();
-});
+  await db.close()
+})
 
 describe('Team Membership routes', () => {
   const teamCaptainPlayerData = {
@@ -31,7 +31,7 @@ describe('Team Membership routes', () => {
     cellphone: '+1234567890',
     password: 'test123',
     role: 'player',
-  };
+  }
   const newPlayerData = {
     playerId: 2,
     name: 'Paul Bunyan',
@@ -39,23 +39,23 @@ describe('Team Membership routes', () => {
     cellphone: '+0987654321',
     password: 'welcome',
     role: 'player',
-  };
+  }
 
   const existingTeamData = {
     teamId: 1,
     name: 'Test Team',
-  };
+  }
 
   const teamCaptainMembershipData = {
     teamId: existingTeamData.teamId,
     playerId: teamCaptainPlayerData.playerId,
     isCaptain: true,
-  };
+  }
 
   beforeEach(async () => {
     const hashedCaptainPassword = await hashPassword(
-      teamCaptainPlayerData.password
-    );
+      teamCaptainPlayerData.password,
+    )
     await db.run(
       'INSERT INTO players (id, name, email, cellphone, password, role) VALUES (?, ?, ?, ?, ?, ?)',
       [
@@ -65,11 +65,11 @@ describe('Team Membership routes', () => {
         teamCaptainPlayerData.cellphone,
         hashedCaptainPassword,
         teamCaptainPlayerData.role,
-      ]
-    );
+      ],
+    )
     const hashedPlayerPassword = await hashPassword(
-      teamCaptainPlayerData.password
-    );
+      teamCaptainPlayerData.password,
+    )
     await db.run(
       'INSERT INTO players (id, name, email, cellphone, password, role) VALUES (?, ?, ?, ?, ?, ?)',
       [
@@ -79,140 +79,237 @@ describe('Team Membership routes', () => {
         newPlayerData.cellphone,
         hashedPlayerPassword,
         newPlayerData.role,
-      ]
-    );
+      ],
+    )
 
     await db.run('INSERT INTO teams (id, name) VALUES (?, ?)', [
       existingTeamData.teamId,
       existingTeamData.name,
-    ]);
+    ])
 
     createTeamMembership(
       db,
       existingTeamData.teamId,
       teamCaptainPlayerData.playerId,
-      true
-    );
-  });
+      true,
+    )
+  })
 
   afterEach(async () => {
-    await db.run('DELETE FROM team_memberships');
-    await db.run('DELETE FROM teams');
-    await db.run('DELETE FROM players');
-  });
+    await db.run('DELETE FROM team_memberships')
+    await db.run('DELETE FROM teams')
+    await db.run('DELETE FROM players')
+  })
 
   it('should create a new team membership', async () => {
     const token = generateToken({
       id: teamCaptainPlayerData.playerId,
       role: teamCaptainPlayerData.role,
-    });
+    })
 
     const res = await supertest(app)
       .post(`/teams/${existingTeamData.teamId}/team-memberships`)
       .send({ playerId: newPlayerData.playerId, isCaptain: false })
-      .set('Authorization', `Bearer ${token}`);
+      .set('Authorization', `Bearer ${token}`)
 
-    expect(res.status).toEqual(201);
-    expect(res.body).toHaveProperty('id');
-    expect(res.body.player_id).toEqual(newPlayerData.playerId);
-    expect(res.body.team_id).toEqual(existingTeamData.teamId);
-    expect(res.body.is_captain).toBeFalsy();
-  });
+    expect(res.status).toEqual(201)
+    expect(res.body).toHaveProperty('id')
+    expect(res.body.player_id).toEqual(newPlayerData.playerId)
+    expect(res.body.team_id).toEqual(existingTeamData.teamId)
+    expect(res.body.is_captain).toBeFalsy()
+  })
 
   it('should read many team memberships', async () => {
     const token = generateToken({
       id: teamCaptainPlayerData.playerId,
       role: teamCaptainPlayerData.role,
-    });
+    })
 
     // Create a second team membership for testing
     createTeamMembership(
       db,
       existingTeamData.teamId,
       newPlayerData.playerId,
-      false
-    );
+      false,
+    )
 
     const res = await supertest(app)
       .get(`/teams/${existingTeamData.teamId}/team-memberships`)
-      .set('Authorization', `Bearer ${token}`);
+      .set('Authorization', `Bearer ${token}`)
 
-    expect(res.status).toEqual(200);
-    expect(res.body).toHaveLength(2);
-    expect(res.body[0].player_id).toEqual(teamCaptainPlayerData.playerId);
-    expect(res.body[0].team_id).toEqual(existingTeamData.teamId);
-    expect(res.body[0].is_captain).toBeTruthy();
-  });
+    expect(res.status).toEqual(200)
+    expect(res.body).toHaveLength(2)
+    expect(res.body[0].player_id).toEqual(teamCaptainPlayerData.playerId)
+    expect(res.body[0].team_id).toEqual(existingTeamData.teamId)
+    expect(res.body[0].is_captain).toBeTruthy()
+  })
 
   it('should read one team membership', async () => {
     const token = generateToken({
       id: teamCaptainPlayerData.playerId,
       role: teamCaptainPlayerData.role,
-    });
+    })
 
     // Get a team membership for testing
-    const { id } = await db.get(
+    const {
+      id,
+    } = await db.get(
       'SELECT id, team_id, player_id, is_captain FROM team_memberships WHERE team_id = ? AND player_id = ?',
-      [existingTeamData.teamId, teamCaptainPlayerData.playerId]
-    );
+      [existingTeamData.teamId, teamCaptainPlayerData.playerId],
+    )
 
     const res = await supertest(app)
       .get(`/teams/${existingTeamData.teamId}/team-memberships/${id}`)
-      .set('Authorization', `Bearer ${token}`);
+      .set('Authorization', `Bearer ${token}`)
 
-    expect(res.status).toEqual(200);
-    expect(res.body.id).toEqual(id);
-    expect(res.body.player_id).toEqual(teamCaptainPlayerData.playerId);
-    expect(res.body.team_id).toEqual(existingTeamData.teamId);
-    expect(res.body.is_captain).toBeTruthy();
-  });
+    expect(res.status).toEqual(200)
+    expect(res.body.id).toEqual(id)
+    expect(res.body.player_id).toEqual(teamCaptainPlayerData.playerId)
+    expect(res.body.team_id).toEqual(existingTeamData.teamId)
+    expect(res.body.is_captain).toBeTruthy()
+  })
 
   it('should update a team membership', async () => {
     const token = generateToken({
       id: teamCaptainPlayerData.playerId,
       role: teamCaptainPlayerData.role,
-    });
+    })
 
     // Get a team membership for testing
-    const { id } = await db.get(
+    const {
+      id,
+    } = await db.get(
       'SELECT id, team_id, player_id, is_captain FROM team_memberships WHERE team_id = ? AND player_id = ?',
-      [existingTeamData.teamId, teamCaptainPlayerData.playerId]
-    );
+      [existingTeamData.teamId, teamCaptainPlayerData.playerId],
+    )
 
     const res = await supertest(app)
       .put(`/teams/${existingTeamData.teamId}/team-memberships/${id}`)
       .send({ playerId: teamCaptainPlayerData.playerId, isCaptain: false })
-      .set('Authorization', `Bearer ${token}`);
+      .set('Authorization', `Bearer ${token}`)
 
-    expect(res.status).toEqual(200);
-    expect(res.body.id).toEqual(id);
-    expect(res.body.player_id).toEqual(teamCaptainPlayerData.playerId);
-    expect(res.body.team_id).toEqual(existingTeamData.teamId);
-    expect(res.body.is_captain).toBeFalsy();
-  });
+    expect(res.status).toEqual(200)
+    expect(res.body.id).toEqual(id)
+    expect(res.body.player_id).toEqual(teamCaptainPlayerData.playerId)
+    expect(res.body.team_id).toEqual(existingTeamData.teamId)
+    expect(res.body.is_captain).toBeFalsy()
+  })
 
   it('should delete a team membership', async () => {
     const token = generateToken({
       id: teamCaptainPlayerData.playerId,
       role: teamCaptainPlayerData.role,
-    });
+    })
 
     // Create a second team membership for testing
     const { id } = await createTeamMembership(
       db,
       existingTeamData.teamId,
       newPlayerData.playerId,
-      false
-    );
+      false,
+    )
 
     const res = await supertest(app)
       .delete(`/teams/${existingTeamData.teamId}/team-memberships/${id}`)
-      .set('Authorization', `Bearer ${token}`);
+      .set('Authorization', `Bearer ${token}`)
 
-    expect(res.status).toEqual(200);
-    expect(res.body.id).toEqual(id);
-    expect(res.body.player_id).toEqual(newPlayerData.playerId);
-    expect(res.body.team_id).toEqual(existingTeamData.teamId);
-    expect(res.body.is_captain).toBeFalsy();
-  });
-});
+    expect(res.status).toEqual(200)
+    expect(res.body.id).toEqual(id)
+    expect(res.body.player_id).toEqual(newPlayerData.playerId)
+    expect(res.body.team_id).toEqual(existingTeamData.teamId)
+    expect(res.body.is_captain).toBeFalsy()
+  })
+
+  it('should not allow deleting a team membership if not the captain or the member', async () => {
+    // Create a new player who is not a member nor the captain of the team
+    const unrelatedPlayerData = {
+      playerId: 3,
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      cellphone: '+111223344',
+      password: 'random123',
+      role: 'player',
+    }
+
+    const hashedUnrelatedPlayerPassword = await hashPassword(
+      unrelatedPlayerData.password,
+    )
+    await db.run(
+      'INSERT INTO players (id, name, email, cellphone, password, role) VALUES (?, ?, ?, ?, ?, ?)',
+      [
+        unrelatedPlayerData.playerId,
+        unrelatedPlayerData.name,
+        unrelatedPlayerData.email,
+        unrelatedPlayerData.cellphone,
+        hashedUnrelatedPlayerPassword,
+        unrelatedPlayerData.role,
+      ],
+    )
+
+    const token = generateToken({
+      id: unrelatedPlayerData.playerId,
+      role: unrelatedPlayerData.role,
+    })
+
+    // Create a team membership for testing
+    const { id } = await createTeamMembership(
+      db,
+      existingTeamData.teamId,
+      newPlayerData.playerId,
+      false,
+    )
+
+    const res = await supertest(app)
+      .delete(`/teams/${existingTeamData.teamId}/team-memberships/${id}`)
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(res.status).toEqual(403)
+  })
+
+  it('should not allow updating a team membership if not the captain or the member', async () => {
+    // Create a new player who is not a member nor the captain of the team
+    const unrelatedPlayerData = {
+      playerId: 3,
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      cellphone: '+111223344',
+      password: 'random123',
+      role: 'player',
+    }
+
+    const hashedUnrelatedPlayerPassword = await hashPassword(
+      unrelatedPlayerData.password,
+    )
+    await db.run(
+      'INSERT INTO players (id, name, email, cellphone, password, role) VALUES (?, ?, ?, ?, ?, ?)',
+      [
+        unrelatedPlayerData.playerId,
+        unrelatedPlayerData.name,
+        unrelatedPlayerData.email,
+        unrelatedPlayerData.cellphone,
+        hashedUnrelatedPlayerPassword,
+        unrelatedPlayerData.role,
+      ],
+    )
+
+    const token = generateToken({
+      id: unrelatedPlayerData.playerId,
+      role: unrelatedPlayerData.role,
+    })
+
+    // Create a team membership for testing
+    const { id } = await createTeamMembership(
+      db,
+      existingTeamData.teamId,
+      newPlayerData.playerId,
+      false,
+    )
+
+    const res = await supertest(app)
+      .put(`/teams/${existingTeamData.teamId}/team-memberships/${id}`)
+      .send({ playerId: unrelatedPlayerData.playerId, isCaptain: false })
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(res.status).toEqual(403)
+  })
+})
